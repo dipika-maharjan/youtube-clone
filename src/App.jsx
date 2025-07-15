15c8d3b0e55c889657f54e87e4165a0b2ebfa43d
Dipika <maharjandipika49@gmail.com>
2025-07-15 12:28:07 +0545

    rapid api added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { useAuth } from "./context/AuthProvider";
 // import './App.css'
 
 function App() {
+  const {loading, data} = useAuth();
+  console.log(loading)
+  console.log(data)
   return (
     <>
       <Navbar />
